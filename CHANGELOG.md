61c27e5a36ae6f41aed9a1af82c5f4d009a6a9f6
Stephen Neal <stephen.neal14@gmail.com>
2022-02-23 12:31:30 -0500

    Migration File Name (#4)
    * FIX issue with 'create_social_table' migration being published as 'create_role_table'
    
    * BUMP CHANGELOG.md
    
    * MAKE 'social' table's `description` column nullable
    
     - bump CHANGELOG.md

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,3 +7,7 @@ All notable changes to `socials` will be documented in this file
 
 ## 0.1.1 - 2022-01-19
 - fix issue with `DatabaseSeeder` ambiguous class resolution errors
+ 
+## 0.1.2 - 2022-02-23
+- fix issue with 'create_social_table' migration being published as 'create_role_table'
+- make 'social' table's `description` column nullable
--- src/Providers/SocialsServiceProvider.php
+++ src/Providers/SocialsServiceProvider.php
@@ -17,7 +17,7 @@ class SocialsServiceProvider extends ServiceProvider
         if (! class_exists('CreateSocialTable')) {
             $this->publishes([
                 __DIR__.'/../../database/migrations/create_social_table.php.stub' => database_path(
-                    'migrations/'.date('Y_m_d_His', time()).'_create_role_table.php'
+                    'migrations/'.date('Y_m_d_His', time()).'_create_social_table.php'
                 ),
             ], 'migration');
         }
